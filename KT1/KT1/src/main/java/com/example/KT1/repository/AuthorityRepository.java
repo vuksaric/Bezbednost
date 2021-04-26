4efdc58f48bda6a45474a695746831bfd1c7667a
Marjana Zalar <marjana.zalar@gmail.com>
2021-04-26 20:14:42 +0200

    #Attack service and controller

--- KT1/KT1/src/main/java/com/example/KT1/repository/AuthorityRepository.java
+++ KT1/KT1/src/main/java/com/example/KT1/repository/AuthorityRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorityRepository extends JpaRepository<Authority, Long> {
     Authority findOneByName(String role_customer);
+
 }
 
